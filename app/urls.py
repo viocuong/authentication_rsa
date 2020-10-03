18352faa080af2330cf35794308462bc11a8bf65
viocuong <cuong1999ads@gmail.com>
2020-10-03 17:27:54 +0700

    up

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+app_name="app"
 urlpatterns=[
     path("",views.index,name='index'),
-    path("<str:name>",views.greet,name="greet")
+    path("<str:name>",views.greet,name="greet"),
+    path("register/",views.register,name="register"),
+    path("login/",views.login,name="login")
 ]
--- app/views.py
+++ app/views.py
@@ -1,19 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse as res
 from Crypto.PublicKey import RSA
+from django.http import request
+from django import forms
 
+class formlogin(forms.Form):
+    email =forms.CharField(label = "Email", max_length=40, required=True,error_messages={'required':'khong duoc de trong'})
+    passWord = forms.CharField(label= " Mật Khẩu", widget= forms.PasswordInput)
+    
 # Create your views here.
-
-
 def index(request):
-    return render(request, "hello/index.html")
+    form = formlogin()
+    return render(request, "app/index.html",{
+        "form":form
+    })
 
 
 def home(request):
     return res("home")
-
-
 def greet(request, name):
-    return render(request, "hello/greet.html", {
+    return render(request, "app/greet.html", {
         "name": name
     })
+    
+def register(request):
+    return res("welcom register")
+def login(request):
+    if request.method == 'POST':
+        form = formlogin(request.POST)
+        if form.is_valid():
+            return res("login success"+ '  '+form.cleaned_data['email'])
+    return res("login fail")
+    #return res(request.POST.get("email"))
